a5e826325cc3de23ed95bbc927831a08f9f5d07c
Pierre-François Giraud <pierrefrancois.giraud@insa-rouen.fr>
2018-05-02 12:11:10 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -20,40 +20,40 @@
                         
                         <div class="form-group">
                             <label for="profil-Pseudo">Pseudo</label>
-                            <input type="text" class="form-control" id="profil-Pseudo" aria-describedby="PseudoHelp" placeholder="Entrez un pseudonyme valide">
+                            <input type="text" name="Pseudo" class="form-control" id="profil-Pseudo" aria-describedby="PseudoHelp" placeholder="Entrez un pseudonyme valide">
                             <small id="PseudoHelp" class="form-text text-muted">Votre pseudo doit faire au moins 5 caractères</small>
                         </div>
                         <div class="form-group">
                             <label for="profil-Password">Mot de passe</label>
-                            <input type="password" class="form-control" id="profil-Password" aria-describedby="PasswordHelp" placeholder="Entrez un mot de passe valide">
+                            <input type="password" name="Password" class="form-control" id="profil-Password" aria-describedby="PasswordHelp" placeholder="Entrez un mot de passe valide">
                             <small id="PasswordHelp" class="form-text text-muted">Votre mot de passse doit faire au moins 5 caractères et contenir une majuscule</small>
                         </div>
                         <div class="form-group">
                             <label for="profil-Email">Adresse Email</label>
-                            <input type="email" class="form-control" id="profil-Email" placeholder="Entrez votre email">
+                            <input type="email" name="Email" class="form-control" id="profil-Email" placeholder="Entrez votre email">
                         </div>
                         <div class="form-group">
                             <label for="profil-Prenom">Prénom</label>
-                            <input type="text" class="form-control" id="profil-Prenom" placeholder="Entrez votre prénom">
+                            <input type="text" name="Prenom" class="form-control" id="profil-Prenom" placeholder="Entrez votre prénom">
                         </div>
                         <div class="form-group">
                             <label for="profil-Nom">Nom</label>
-                            <input type="text" class="form-control" id="profil-Nom" placeholder="Entrez votre nom de famille">
+                            <input type="text" name="Nom" class="form-control" id="profil-Nom" placeholder="Entrez votre nom de famille">
                         </div>
                         <div class="form-group">
                             <label for="profil-Genre">Sexe</label>
-                            <select name="genre" class="form-control" id="profil-Genre">
+                            <select name="Genre" class="form-control" id="profil-Genre">
                                <option value="0">Homme</option>
                                <option value="1">Femme</option>
                                <option value="2">Un peu des deux</option>
                            </select>
                         </div>
                         <div class="form-group">
-                                <label for="profil-Ville">Ville</label>
-                            <input type="text" class="form-control" id="profil-Ville" placeholder="Entrez votre ville de résidence">
+                            <label for="profil-Ville">Ville</label>
+                            <input type="text" name="Ville" class="form-control" id="profil-Ville" placeholder="Entrez votre ville de résidence">
                         </div>
                         <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="profil-checkCU">
+                            <input type="checkbox" name="checkCU" class="form-check-input" id="profil-checkCU">
                             <label class="form-check-label" for="profil-checkCU">J'accepte les conditions d'utilisation de TinderASI</label>
                         </div>
                         <button type="submit" class="btn btn-primary">Suivant</button>
