32eae74507288df3579939c693e87c26ba4ace82
Pierre-François Giraud <pierrefrancois.giraud@insa-rouen.fr>
2018-05-02 18:43:56 +0200

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -85,4 +85,20 @@ Route::group(['middleware' => ['web']], function () {
 	    
         return redirect('/');
     });
+	
+    Route::get('/resources/views/images/{filename}', function($filename){
+	$path = resource_path() . '/views/images/' . $filename;
+
+	if(!File::exists($path)) {
+		return response()->json(['message' => 'Image not found.'], 404);
+	}
+
+	$file = File::get($path);
+	$type = File::mimeType($path);
+
+	$response = Response::make($file, 200);
+	$response->header("Content-Type", $type);
+
+	return $response;
+    });
 });
