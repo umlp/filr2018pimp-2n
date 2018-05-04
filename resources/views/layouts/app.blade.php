8cd3923ebab5ed6db0a76f28b7f21476c82eab6a
Pierre-François Giraud <pierrefrancois.giraud@insa-rouen.fr>
2018-05-04 19:45:14 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="{{ url('inscription_1') }}"><span class="glyphicon glyphicon-user"></span>Inscription</a></li>
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
