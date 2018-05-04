bd17ece3b608e52e19047945c79f022c377159a5
Pierre-François Giraud <pierrefrancois.giraud@insa-rouen.fr>
2018-05-04 21:48:55 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -12,7 +12,7 @@
                     </tr>
                     </table>
                 </div>
-                <div class="panel-body" style="margin:5px;">
+                <div class="panel-body" style="margin:2%;">
                     <!-- Display Validation Errors -->
                     @include('common.errors')
                     @if (isset($accueil))
